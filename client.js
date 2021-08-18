755490302ae707541eb9b0b937058dc842f26ac6
esra-saatci <esra.saatci86@gmail.com>
2021-08-18 20:04:54 +0000

    Adds move commands with setInterval

--- client.js
+++ client.js
@@ -1,25 +1,38 @@
 const net = require('net');
 
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
+
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  // attach an event handler to handle incoming data 
-  
-  conn.on('connect', () => {
-    console.log("Successfully connected to game server")
-    conn.write('Name: ES');
+  conn.setEncoding('utf8');
+
+  // attach an event handler to handle incoming data
+  conn.on('data', (data) => {
+    console.log(data);
   });
 
-  conn.on('data', (data) => {
-    console.log(data)
+  // Connect event handler
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: ES');
+
+    // const intervalID = setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 500);
+
+    // //Use setTimeout to stop the snake after 6 moves
+    // setTimeout(() => {
+    //   clearInterval(intervalID);
+    // },3000);
+  
   });
+
 
   return conn;
-}
+};
 
 
 module.exports = {connect};
