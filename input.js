21d8b16a37f634d83093b4839c9e1ef0e06ec6ec
esra-saatci <esra.saatci86@gmail.com>
2021-08-19 01:12:39 +0000

    Sends canned messages to the server

--- input.js
+++ input.js
@@ -7,7 +7,7 @@ const setupInput = function(conn) {
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on("data", key => {
-    handleUserInput(key)
+    handleUserInput(key);
   });
 
   return stdin;
@@ -32,37 +32,13 @@ const handleUserInput = function(key) {
   } else if (key === "d") {
     // console.log("Move : right")
     connection.write("Move: right");
+  } else if (key === "q") {
+    connection.write("Say: Oops, wrong key!");
+  } else if (key === "x") {
+    connection.write("Say: Focus!");
   }
 };
   
 
 
 module.exports = {setupInput};
-
-
-
-
-// /**
-//  * Setup User Interface
-//  * Specifically, so that we can handle user input via stdin
-//  */
-//  const setupInput = function() {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding('utf8');
-//   stdin.resume();
-//   stdin.on("data", handleUserInput);
-//   return stdin;
-// };
-
-
-// // Handling CTRL + C User Input
-
-// const handleUserInput = function(key) {
-// // \u0003 maps to ctrl+c input
-//   if (key === '\u0003') {
-//     process.exit();
-//   }
-// };
-
-// module.exports = {setupInput};
